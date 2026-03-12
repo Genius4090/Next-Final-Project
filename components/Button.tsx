4ceece9de0c56955a9c59a5ba4fc0219e63d86d2
genius4090 <abdulboriytolqinov00200@gmail.com>
2026-03-12 14:49:29 +0500

    feat: initial commit

---
+++ components/Button.tsx
@@ -0,0 +1,10 @@
+// interface Button
+
+
+// const Button = ({LeftIcon,title,RightIcon,onClick,extraStyle}) => {
+//   return (
+//     <button className={`bg-black text-white py-1 px-4 ${extraStyle}`} onClick={onClick}>{LeftIcon}{title}{RightIcon}</div>
+//   )
+// }
+
+// export default Button
